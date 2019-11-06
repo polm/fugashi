14108ec2738d4eb0f86122eb24f4e564aed39754
Paul O'Leary McCann <polm@dampfkraft.com>
2019-11-06 13:43:45 +0900

    Add basic tests

---
+++ fugashi/tests/test_basic.py
@@ -0,0 +1,27 @@
+from fugashi import Tagger
+import pytest
+
+WAKATI_TESTS = (
+        ("すももももももももの内", 'すもも も もも も もも の 内 \n'),
+        ("日本語ですよ", '日本 語 です よ \n'),
+        ("深海魚は、深海に生息する魚類の総称。", '深海 魚 は 、 深海 に 生息 する 魚類 の 総称 。 \n'),
+        )
+
+TOKENIZER_TESTS = (
+        ('あなたは新米の魔女。', ['<BOS>', 'あなた', 'は', '新米', 'の', '魔女', '。', '<EOS>']),
+        ('パートナーである猫と共に、見知らぬ町へやってきたばかりです。', ['<BOS>', 'パートナー', 'で', 'ある', '猫', 'と', '共', 'に', '、', '見知ら', 'ぬ', '町', 'へ', 'やっ', 'て', 'き', 'た', 'ばかり', 'です', '。', '<EOS>']),
+
+        )
+
+@pytest.mark.parametrize('text,wakati', WAKATI_TESTS)
+def test_wakati(text, wakati):
+    tagger = Tagger('-Owakati')
+    assert tagger.parse(text) == wakati
+
+@pytest.mark.parametrize('text,saved', TOKENIZER_TESTS)
+def test_tokens(text, saved):
+    # testing the token objects is tricky, so instead just check surfaces
+    #TODO: maybe save serialized nodes to compare?
+    tagger = Tagger()
+    tokens = [str(tok) for tok in tagger.parseToNodeList(text)]
+    assert tokens == saved
