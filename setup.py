6b9c41f3f03c174fd37ef54fa1eb877635668886
Deyong Zheng <zhengdy@me.com>
2019-11-28 15:38:25 +0900

    add Cython to setup_requires and install_requires.

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
+import pathlib
 import setuptools
 from distutils.core import setup
 
 from distutils.extension import Extension
-from Cython.Build import cythonize
 
 extensions = Extension('fugashi', 
         ['fugashi/fugashi.pyx'], 
         libraries=['mecab'])
 
-with open('README.md') as ff:
-    README = ff.read()
 setup(name='fugashi', 
       version='0.1.3',
       author="Paul O'Leary McCann",
       author_email="polm@dampfkraft.com",
       description="A Cython wrapper for MeCab",
-      long_description= README,
+      long_description=pathlib.Path('README.md').read_text(),
       long_description_content_type="text/markdown",
       url="https://github.com/polm/fugashi",
       packages=setuptools.find_packages(),
+      install_requires=['Cython'],
       classifiers=[
           "License :: OSI Approved :: MIT License",
           "Natural Language :: Japanese",
           ],
       python_requires='>=3.6',
-      ext_modules=cythonize(extensions),
-      setup_requires=['wheel'])
+      ext_modules=[extensions],
+      setup_requires=['wheel', 'Cython'])
