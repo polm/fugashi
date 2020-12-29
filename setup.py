74e340fe79fcc35563810432be6d68bf488b2f8f
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2020-12-29 17:10:04 +0100

    fix #29: restore setup.py --version by using setuptools-setup (#30)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import pathlib
 import setuptools
 import subprocess
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 
 from fugashi_util import check_libmecab
