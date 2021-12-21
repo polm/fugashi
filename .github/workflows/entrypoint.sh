ba7e679a2872aee9b96743447827bd3aee53ef73
lambdadog <github@pea.sh>
2021-12-20 21:46:40 -0500

    Switch to only 3.7 and up
    Anki doesn't support python versions below 3.7

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -24,7 +24,7 @@ make install
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 
 # Build the wheels
-Python="cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310"
+Python="cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310"
 for PYVER in $Python; do
   # install cython first
   /opt/python/$PYVER/bin/pip install cython setuptools-scm
