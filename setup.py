c0aeea33de91dc2ab22968573da63ecb4c02d609
Paul O'Leary McCann <polm@dampfkraft.com>
2024-10-31 15:01:12 +0900

    Update Cython version
    Cython actually did jump from 0.29 to 3.0, there are no versions in
    between.

--- setup.py
+++ setup.py
@@ -56,4 +56,4 @@ setup(name='fugashi',
           'unidic': ['unidic'],
           'unidic-lite': ['unidic-lite'],
       },
-      setup_requires=['wheel', 'Cython~=0.29.35', 'setuptools_scm'])
+      setup_requires=['wheel', 'Cython~=3.0.11', 'setuptools_scm'])
