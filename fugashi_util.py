d28089bb2c1231799cc73c84b8669781a7bb15ff
Paul O'Leary McCann <polm@dampfkraft.com>
2025-05-26 15:53:39 +0900

    Use black formatting

--- fugashi_util.py
+++ fugashi_util.py
@@ -4,30 +4,41 @@ import subprocess
 
 
 def mecab_config(com="mecab-config"):
-    output = subprocess.check_output([com, "--inc-dir", "--libs-only-L", "--libs-only-l"])
+    output = subprocess.check_output(
+        [com, "--inc-dir", "--libs-only-L", "--libs-only-l"]
+    )
     if not isinstance(output, str):
         output = output.decode("utf-8")
-    return output.split('\n'), []
+    return output.split("\n"), []
+
 
 def mecab_config_windows():
     ## Windows
-    if not os.name == 'nt':
+    if not os.name == "nt":
         return
 
-    win_mecab_dir = r'C:\mecab'
-    win_bin_dir = win_mecab_dir # this is separate from the sdk dir on some installs
-    mecab_details = (win_mecab_dir, win_mecab_dir, 'libmecab')
-    data_files = ['{}\\libmecab.dll'.format(win_bin_dir)]
+    win_mecab_dir = r"C:\mecab"
+    win_bin_dir = win_mecab_dir  # this is separate from the sdk dir on some installs
+    mecab_details = (win_mecab_dir, win_mecab_dir, "libmecab")
+    data_files = ["{}\\libmecab.dll".format(win_bin_dir)]
     return mecab_details, data_files
+
 
 def mecab_config_cygwin():
     ## Cygwin
     os.chdir("build/mecab")
     if platform.system().startswith("CYGWIN"):
         rep = "mecab-cygwin64" if platform.machine() == "x86_64" else "mecab-cygwin32"
-        subprocess.run(["git", "clone", "--depth=1", "https://github.com/KoichiYasuoka/"+rep])
-        mecab_details = ("build/mecab/"+rep+"/include", "build/mecab/"+rep+"/lib", "mecab stdc++")
+        subprocess.run(
+            ["git", "clone", "--depth=1", "https://github.com/KoichiYasuoka/" + rep]
+        )
+        mecab_details = (
+            "build/mecab/" + rep + "/include",
+            "build/mecab/" + rep + "/lib",
+            "mecab stdc++",
+        )
         return mecab_details, []
+
 
 def check_libmecab():
     """Get MeCab build parameters.
@@ -36,10 +47,10 @@ def check_libmecab():
     will be installed or the parameters will otherwise be figured out."""
 
     configs = [
-            mecab_config_windows,
-            mecab_config,
-            mecab_config_cygwin,
-            ]
+        mecab_config_windows,
+        mecab_config,
+        mecab_config_cygwin,
+    ]
 
     # A few scripts will use a build directory. Save where we start so we can
     # reset the directory after each build step.
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from fugashi_util import check_libmecab
 output, dll_files = check_libmecab()
 
 # pad the list in case something's missing
-mecab_config = list(output) + ([''] * 5)
+mecab_config = list(output) + ([""] * 5)
 include_dirs = mecab_config[0].split()
 library_dirs = mecab_config[1].split()
 libraries = mecab_config[2].split()
@@ -25,60 +25,67 @@ extra_link_args = mecab_config[4].split()
 # an env var.
 bundle_dll = False
 fugashi_package_files = []
-no_bundle_env_var = os.environ.get('FUGASHI_NO_BUNDLE_DLL', '')
-if sys.platform == 'win32' and no_bundle_env_var not in ['', '0']:
+no_bundle_env_var = os.environ.get("FUGASHI_NO_BUNDLE_DLL", "")
+if sys.platform == "win32" and no_bundle_env_var not in ["", "0"]:
     bundle_dll = True
     fugashi_package_files = [pathlib.Path(i).name for i in dll_files]
+
 
 class build_ext(_build_ext):
     """Custom behavior for build_ext.
 
     This is only run when bundling DLLs on Windows, which requires copying
     files around."""
+
     def run(self):
         if bundle_dll:
             if self.editable_mode:
-                fugashi_dir = pathlib.Path(__file__).parent / 'fugashi'
+                fugashi_dir = pathlib.Path(__file__).parent / "fugashi"
             else:
-                fugashi_dir = pathlib.Path(self.build_lib) / 'fugashi'
+                fugashi_dir = pathlib.Path(self.build_lib) / "fugashi"
             for i in dll_files:
                 self.copy_file(i, fugashi_dir)
         return super().run()
 
 
-extensions = Extension('fugashi.fugashi', 
-        ['fugashi/fugashi.pyx'], 
-        libraries=libraries,
-        library_dirs=library_dirs,
-        include_dirs=include_dirs,
-        extra_objects=extra_objects,
-        extra_link_args=extra_link_args)
+extensions = Extension(
+    "fugashi.fugashi",
+    ["fugashi/fugashi.pyx"],
+    libraries=libraries,
+    library_dirs=library_dirs,
+    include_dirs=include_dirs,
+    extra_objects=extra_objects,
+    extra_link_args=extra_link_args,
+)
 
-setup(name='fugashi', 
-      use_scm_version=True,
-      author="Paul O'Leary McCann",
-      author_email="polm@dampfkraft.com",
-      description="A Cython MeCab wrapper for fast, pythonic Japanese tokenization.",
-      long_description=pathlib.Path('README.md').read_text('utf8'),
-      long_description_content_type="text/markdown",
-      url="https://github.com/polm/fugashi",
-      packages=setuptools.find_packages(),
-      classifiers=[
-          "License :: OSI Approved :: MIT License",
-          "Natural Language :: Japanese",
-          ],
-      python_requires='>=3.8',
-      ext_modules=[extensions],
-      cmdclass={'build_ext': build_ext},
-      package_data={'fugashi': fugashi_package_files},
-      entry_points={
-          'console_scripts': [
-              'fugashi = fugashi.cli:main',
-              'fugashi-info = fugashi.cli:info',
-              'fugashi-build-dict = fugashi.cli:build_dict',
-      ]},
-      extras_require={
-          'unidic': ['unidic'],
-          'unidic-lite': ['unidic-lite'],
-      },
-      setup_requires=['wheel', 'Cython~=3.0.11', 'setuptools_scm'])
+setup(
+    name="fugashi",
+    use_scm_version=True,
+    author="Paul O'Leary McCann",
+    author_email="polm@dampfkraft.com",
+    description="A Cython MeCab wrapper for fast, pythonic Japanese tokenization.",
+    long_description=pathlib.Path("README.md").read_text("utf8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/polm/fugashi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Japanese",
+    ],
+    python_requires=">=3.8",
+    ext_modules=[extensions],
+    cmdclass={"build_ext": build_ext},
+    package_data={"fugashi": fugashi_package_files},
+    entry_points={
+        "console_scripts": [
+            "fugashi = fugashi.cli:main",
+            "fugashi-info = fugashi.cli:info",
+            "fugashi-build-dict = fugashi.cli:build_dict",
+        ]
+    },
+    extras_require={
+        "unidic": ["unidic"],
+        "unidic-lite": ["unidic-lite"],
+    },
+    setup_requires=["wheel", "Cython~=3.0.11", "setuptools_scm"],
+)
